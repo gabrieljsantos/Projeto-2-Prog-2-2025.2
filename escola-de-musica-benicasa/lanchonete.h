dc10c5872a8e0f61a951971eb7885c5774c0f413
GustavoBastos1 <motagustavo039@gmail.com>
2026-02-13 00:46:33 -0300

    Implement adicionarproduto function to save products

--- escola-de-musica-benicasa/lanchonete.h
+++ escola-de-musica-benicasa/lanchonete.h
@@ -11,4 +11,27 @@ void bancodedadoslanchonete();
 void menuCadastroProdutos();
 void adicionarCreditosUsuario();
 void consultarEstoque();
-void adicionarproduto();
+void adicionarproduto(){
+    Produto p;
+    ofstream arquivo("produtos.dat", ios::binary | ios::app);
+    if(arquivo.is_open()){
+        cout << "ID: ";
+        cin >> p.id;
+        cout << "Nome: ";
+        cin.ignore();
+        cin.getline(p.nome, 50);
+        cout << "Preco: ";
+        cin >> p.preco;
+        cout << "Estoque: ";
+        cin >> p.estoque;
+        p.ativo = 1;
+
+        arquivo.write((char*)&p, sizeof(Produto));
+        arquivo.close();
+         cout << "Produto salvo com sucesso!\n";
+    }
+    else{
+        cout<<"Erro ao abrir o arquivo!\n";
+    }
+    arquivo.close();
+}
