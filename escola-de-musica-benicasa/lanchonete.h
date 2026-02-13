b2610da72571deeae0c95d6257f9128da1e8e628
GustavoBastos1 <motagustavo039@gmail.com>
2026-02-13 20:38:08 -0300

    Refactor lanchonete.h by removing and declaring functions

--- escola-de-musica-benicasa/lanchonete.h
+++ escola-de-musica-benicasa/lanchonete.h
@@ -1,37 +1,14 @@
 #include "headers.h"
+#include <fstream>
 
 
-bool verificaratividade(usuario pessoa){
-    if(pessoa.ativo==1)
-        return true;
-    else
-        return false;
-}
+bool verificaratividade(Usuario pessoa);//Feito
 void bancodedadoslanchonete();
 void menuCadastroProdutos();
 void adicionarCreditosUsuario();
 void consultarEstoque();
-void adicionarproduto(){
-    Produto p;
-    ofstream arquivo("produtos.dat", ios::binary | ios::app);
-    if(arquivo.is_open()){
-        cout << "ID: ";
-        cin >> p.id;
-        cout << "Nome: ";
-        cin.ignore();
-        cin.getline(p.nome, 50);
-        cout << "Preco: ";
-        cin >> p.preco;
-        cout << "Estoque: ";
-        cin >> p.estoque;
-        p.ativo = 1;
-
-        arquivo.write((char*)&p, sizeof(Produto));
-        arquivo.close();
-         cout << "Produto salvo com sucesso!\n";
-    }
-    else{
-        cout<<"Erro ao abrir o arquivo!\n";
-    }
-    arquivo.close();
-}
+void adicionarproduto();//Feito
+void removerproduto();
+void fazercompra();
+void removercredito();//Feito
+void visualizarProdutos();//Feito
