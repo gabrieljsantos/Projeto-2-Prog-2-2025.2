b5cc8a08aab9db408a0f231ccc762fb1ae4a8a9a
Ulisses <ufsbibuli@gmail.com>
2026-02-14 20:14:50 -0300

    Add instrument management functions and structure

--- escola-de-musica-benicasa/instrumentos.h
+++ escola-de-musica-benicasa/instrumentos.h
@@ -1,1 +1,14 @@
+#ifndef INSTRUMENTOS_H
+#define INSTRUMENTOS_H
+void menuCadastroInstrumentos();
+void deleteInstrumentos();
+void mostrarInstrumentosCadastrados();
+void openFile();
+struct Instrumentos{
+    int id;
+    char nome[20];
+    bool disponivel;
+    char responsavel[50];
+};
+#endif
 
