dc0ea5f1fff02dc6776ad7a2a4a1d4b1b99b4ee0
edusts-SI <edusts82@gmail.com>
2026-02-20 19:08:54 -0300

    Refatorar opções de gerenciamento de eventos em eventos.cpp

--- escola-de-musica-benicasa/eventos.cpp
+++ escola-de-musica-benicasa/eventos.cpp
@@ -22,13 +22,8 @@ int main()
         while (opcao != 0)
         {
             cout << "\n--------Gerenciamento de Eventos da Escola--------\n";
-            cout << "1 - Criar Novo Evento\n";
-            cout << "2 - Editar Evento\n";
-            cout << "3 - Listar Eventos Agendados\n";
-            cout << "4 - Visualizar Inscritos em Evento\n";
-            cout << "5 - Excluir Evento\n";
-            cout << "6 - Listar alunos insritos\n";
-            cout << "0 - Voltar\n";
+            cout << "1 - Listar Eventos\n";
+            cout << "2 - Inscrever-se\n";
             cout << "Escolha uma opcao: ";
             cin >> opcao;
             cin.ignore();
@@ -36,29 +31,14 @@ int main()
             switch (opcao)
             {
                 case 1:
-                    adicionar_evento();
+                     listar_eventos(eventos);
                     break;
                 case 2:
-                    editar_evento (eventos);
+                    inscrever_alunos();
                     break;
-                case 3:
-                    listar_eventos(eventos);
-                    break;
-                case 4:
-                    listar_alunos_inscritos();
-                    break;
-                case 5:
-                {
-                    int idExc;
-                    cout << "Digite o id do evento que você quer excluir: \n";
-                    cin >> idExc;
-                    excluir_evento(idExc);
-                    break;
-                }
-
                 case 0:
                     exit(1);
-
+                }
                 default:
                     cout << "Opcao invalida. Tente novamente.\n";
 
@@ -66,6 +46,3 @@ int main()
         }
         return 0;
 }
-
-Recolher (69 linhas)
-
