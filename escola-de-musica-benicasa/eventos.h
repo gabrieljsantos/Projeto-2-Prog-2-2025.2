3d2b46180ac70946ccd91e475f25fe18ebbf4cd6
edusts-SI <edusts82@gmail.com>
2026-02-20 22:40:33 -0300

    Adicionar funções de gerenciamento de eventos e variáveis ​​globais.

--- escola-de-musica-benicasa/eventos.h
+++ escola-de-musica-benicasa/eventos.h
@@ -1,1 +1,21 @@
+#ifndef EVENTOS_H
+#define EVENTOS_H
 
+#include <vector>
+#include "headers.h" // Importa as estruturas centralizadas
+
+// Variáveis globais compartilhadas (a definição real fica no .cpp)
+extern std::vector<Evento> eventos;
+extern int proximoID;
+
+// Declaração das Funções do módulo
+bool SalvarEventosNoArquivo(const std::vector<Evento>& eventos);
+void CarregarEventos();
+void adicionar_evento();
+void editar_evento(std::vector<Evento>& eventos);
+void excluir_evento(int id_excluir);
+void listar_alunos_inscritos();
+void listar_eventos(const std::vector<Evento>& eventos);
+void inscrever_alunos();
+
+#endif // EVENTOS_H
